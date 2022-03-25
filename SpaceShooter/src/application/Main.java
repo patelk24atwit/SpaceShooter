06842c2e2faf359d9ddb6b8c8996b0d039b1e132
Deuos <74274171+Deuos@users.noreply.github.com>
2022-03-25 14:44:17 -0400

    Scene

--- SpaceShooter/src/application/Main.java
+++ SpaceShooter/src/application/Main.java
@@ -3,25 +3,52 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,500,600);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	//This works
+	
+	Stage window;
+	Scene scene1, scene2;
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		window = primaryStage;
+		
+		//button1 
+		
+		Button button1 = new Button("Click to Scene 2");
+		button1.setOnAction(e -> window.setScene(scene2));
+		
+		//Layout 1 - 
+		
+		StackPane layout1 = new StackPane();
+		layout1.getChildren().addAll(button1);
+		scene1 = new Scene(layout1, 500, 300);
+		
+		//button 2
+		Button button2 = new Button("This scene 2 go to Scene 1");
+		button2.setOnAction(e -> window.setScene(scene1));
+		
+		
+		//layout 2
+		
+		StackPane layout2 = new StackPane();
+		layout2.getChildren().addAll(button2);
+		scene2 = new Scene(layout2, 500, 300);
+		
+		window.setScene(scene1);
+		window.setTitle("Scene1");
+		window.show();
+	}
+	//This works
+	
 }
