61e4e1e50da56e0bac189556d32d56227c50bef1
sawyers1 <sawyers1@CND122052F.wit.private>
2022-04-04 09:46:12 -0400

    hi

---
+++ SpaceShooter/src/application/Asteroids.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Asteroids {
+	// create the asteroid
+	Image asteroid = new Image("https://www.kindpng.com/picc"
+			+ "/m/255-2559955_preview-asteroid-transparent-background-hd-png-download.png");
+	ImageView imgAsteroid = new ImageView(asteroid);
+	
+	// 
+
+}
---
+++ SpaceShooter/src/application/Shapes.java
@@ -0,0 +1,35 @@
+package application;
+
+import javafx.geometry.Point2D;
+import javafx.scene.shape.Polygon;
+
+/**
+ * 
+ * this is an abstract class used to define the movements 
+ * of the general shapes in the game
+ * it supercedes the asteroid and spaceship class
+ *
+ */
+public abstract class Shapes {
+	
+	private Point2D moveShape;
+	private Polygon shape;
+	
+	/**
+	 * 
+	 * creates a general polygon that has an x and y
+	 * in the pane space that can be translated
+	 * 
+	 * 
+	 */
+	public Shapes (Polygon general, double x, double y) {
+		this.shape = general;
+		this.shape.setTranslateX(x);
+		this.shape.setTranslateY(y);
+		this.moveShape = new Point2D(0.,0.);
+	}
+	
+	
+	
+	
+}
