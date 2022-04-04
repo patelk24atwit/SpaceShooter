8e02a8ec7dd3b5dec5b11155918c063633aefaba
sawyers1 <sawyers1@CND122052F.wit.private>
2022-04-04 09:49:25 -0400

    adding constructors?

--- SpaceShooter/src/application/Asteroids.java
+++ SpaceShooter/src/application/Asteroids.java
@@ -2,13 +2,14 @@ package application;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Polygon;
 
-public class Asteroids {
-	// create the asteroid
-	Image asteroid = new Image("https://www.kindpng.com/picc"
-			+ "/m/255-2559955_preview-asteroid-transparent-background-hd-png-download.png");
-	ImageView imgAsteroid = new ImageView(asteroid);
+public class Asteroids extends Shapes {
+
+	public Asteroids(Polygon general, double x, double y) {
+		super(general, x, y);
+		// TODO Auto-generated constructor stub
+	}
 	
-	// 
 
 }
--- SpaceShooter/src/application/Spaceship.java
+++ SpaceShooter/src/application/Spaceship.java
@@ -1,5 +1,13 @@
 package application;
 
-public class Spaceship {
+import javafx.scene.shape.Polygon;
 
+public class Spaceship extends Shapes{
+	
+	public Spaceship(Polygon general, double x, double y) {
+		super(general, x, y);
+	
+	}
+
+	
 }
